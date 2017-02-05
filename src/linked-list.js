f9937e4bac10d07cd087e8777aa5092e96a512c6
ivrik <ivrik14@gmail.com>
2017-02-05 23:08:05 +0300

    linkedlist:changes

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,141 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
 
-    append(data) {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
+    }
 
-    head() {}
+    append(data) {
+        let thing = new Node(data);
 
-    tail() {}
+        if(this.length === 0){
+            this._head = thing;
+            this._tail = thing;
+        }else{
 
-    at(index) {}
+        }
 
-    insertAt(index, data) {}
+        this._tail.next = thing;
+        thing.prev = this._tail;
+        this._tail = thing;
 
-    isEmpty() {}
+        this.length ++;
+        return this;
+    }
 
-    clear() {}
+    head() {
+        return this._head ?  this._head.data :null;
+    }
 
-    deleteAt(index) {}
+    tail() {
+        return this._tail ?  this._tail.data :null;
+    }
 
-    reverse() {}
+    at(index) {
+        if(index > this.length || index < 0){
+            throw "Invalid index";
+        }
 
-    indexOf(data) {}
+        let thing = this._head;
+        let a = 0;
+
+        while(a<index){
+            thing = thing.next;
+            a++;
+        }
+
+        return thing.data;
+    }
+
+    insertAt(index, data) {
+        if(index < 0){
+            throw "Invalid index";
+        }
+
+        if(this.isEmpty()){
+            this.append(data);
+        }
+
+
+
+        let thing = this._head;
+        let a = 0;
+
+        while(a<index){
+            thing = thing.next;
+            a++;
+        }
+
+        let newNode = new Node(data, thing.prev, thing);
+
+        thing.prev.next = newNode;
+        thing.prev = newNode;
+
+        return this;
+    }
+
+    isEmpty() { return this.length===0;}
+
+    clear() {
+        this.length = 0;
+        this._head =  null;
+        this._tail = null;
+        return this;
+    }
+
+    deleteAt(index) {
+        if(index < 0){
+            throw "Invalid index";
+        }
+
+
+        let thing = this._head;
+        let a = 0;
+
+        while(a<index){
+            thing = thing.next;
+            a++;
+        }
+        thing.prev.next = thing.next;
+        thing.next.prev = thing.prev;
+        return this;
+    }
+
+    reverse() {
+        let newNode = this._tail;
+        this._tail = this._head;
+        this._head = newNode;
+
+
+        while (newNode.prev!=this._tail){
+            let temp = newNode.prev;
+            newNode.prev = newNode.next;
+            newNode.next = temp;
+            newNode = temp;
+        }
+        return this;
+    }
+
+    indexOf(data) {
+
+
+        let thing = this._head;
+        let a = 0;
+
+        do{
+            if(thing.data === data){
+                return a;
+            }
+            thing = thing.next;
+            a++;
+        }
+        while(thing.next!==this._tail);
+
+        return -1;
+    }
 }
 
 module.exports = LinkedList;
